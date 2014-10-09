43acf535e7d2251e568fb63d86fc6521ea109ec2
Ethan Thacker <ethacker@uncc.edu>
2014-10-09 01:14:53 -0300

    made some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Myrottenpotatoes:Application.routes.draw do
-resources:movies
-root:to=>redirect('/movies')
+Myrottenpotatoes::Application.routes.draw do
+resources :movies
+root :to=>redirect('/movies')
 
 end
